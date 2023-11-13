7fb923e1544e81966e1efc00d078833a207f1b97
znehraks <znehraks@gmail.com>
2023-11-14 02:05:27 +0900

    10,11 스크롤 이벤트로 변화하는 역동적인 효과 추가하기

--- chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
+++ chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
@@ -9,7 +9,7 @@ import {
   useTexture,
 } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
@@ -17,10 +17,16 @@ import gsap from "gsap";
 import * as THREE from "three";
 
 let timeline;
+const colors = {
+  boxMaterialColor: "#DC4F00",
+  // currentAnimation: "wave",
+};
 export const Dancer = () => {
   const isEntered = useRecoilValue(IsEnteredAtom);
   const three = useThree();
   const scroll = useScroll(); // ScrollControls 하위에 있는 컴포넌트가 사용 가능한 hook
+  const [currentAnimation, setCurrentAnimation] = useState("wave");
+  const [rotateFinished, setRotateFinished] = useState(false);
   const { scene, animations } = useGLTF("/models/dancer.glb"); // useGLTF Hook으로 모델링 로드
   const texture = useTexture("/textures/5.png");
   const dancerRef = useRef(null);
@@ -45,14 +51,54 @@ export const Dancer = () => {
   useFrame(() => {
     if (!isEntered) return;
     timeline.seek(scroll.offset * timeline.duration());
+    boxRef.current.material.color = new THREE.Color(colors.boxMaterialColor);
+    if (rotateFinished) {
+      setCurrentAnimation("breakdancingEnd");
+    } else {
+      setCurrentAnimation("wave");
+    }
     // console.log(scroll.offset); // scroll.offset을 통해, 현재 스크롤된 offset을 알 수 있다.(스크롤을 하나도 안하면 0, 끝까지 하면 1)
   });
 
+  // 초기화
   useEffect(() => {
     if (!isEntered) return;
-    actions["wave"].play(); // 'wave'라는 이름의 애니메이션 재생
-  }, [actions, isEntered]);
+    three.camera.lookAt(1, 2, 0);
+    actions["wave"]?.play();
+    three.scene.background = new THREE.Color(colors.boxMaterialColor);
+    scene.traverse((obj) => {
+      if (obj.isMesh) {
+        obj.castShadow = true;
+        obj.receiveShadow = true;
+      }
+    });
+  }, [actions, isEntered, scene, three.camera, three.scene]);
 
+  // 애니메이션 컨트롤
+  useEffect(() => {
+    let timeout;
+    if (currentAnimation === "wave") {
+      actions[currentAnimation]?.reset().fadeIn(0.5).play();
+    } else {
+      actions[currentAnimation]
+        ?.reset()
+        .fadeIn(0.5)
+        .play()
+        .setLoop(THREE.LoopOnce, 1);
+
+      timeout = setTimeout(() => {
+        if (actions[currentAnimation]) {
+          actions[currentAnimation].paused = true;
+        }
+      }, 8000);
+    }
+    return () => {
+      clearTimeout(timeout);
+      actions[currentAnimation]?.reset().fadeOut(0.5).stop();
+    };
+  }, [actions, currentAnimation, scroll.offset]);
+
+  // gsap 초기 카메라 회전 및 배경 별 반짝임 애니메이션
   useEffect(() => {
     if (!isEntered) return;
     if (!dancerRef.current) return;
@@ -80,11 +126,52 @@ export const Dancer = () => {
         z: 0,
       }
     );
+
+    gsap.fromTo(
+      colors,
+      { boxMaterialColor: "#0C0400" },
+      {
+        duration: 2.5,
+        boxMaterialColor: "#DC4F00",
+      }
+    );
+
+    gsap.to(starGroupRef01.current, {
+      yoyo: true,
+      duration: 2,
+      repeat: -1,
+      ease: "linear",
+      size: 0.05,
+    });
+
+    gsap.to(starGroupRef02.current, {
+      yoyo: true,
+      duration: 3,
+      repeat: -1,
+      // ease: "power1.inOut",
+
+      ease: "linear",
+      size: 0.05,
+    });
+
+    gsap.to(starGroupRef03.current, {
+      yoyo: true,
+      duration: 4,
+      repeat: -1,
+      // ease: "elastic.in",
+
+      ease: "linear",
+      size: 0.05,
+    });
   }, [isEntered, three.camera.position, three.camera.rotation]);
 
+  // 스크롤 시, 일어날 애니메이션 컨트롤
   useEffect(() => {
-    if (!isEntered) return;
     if (!dancerRef.current) return;
+    const pivot = new THREE.Group();
+    pivot.position.copy(dancerRef.current.position);
+    pivot.add(three.camera);
+    three.scene.add(pivot);
     timeline = gsap.timeline();
     timeline
       .from(
@@ -112,6 +199,28 @@ export const Dancer = () => {
         },
         "<"
       )
+      .to(
+        colors,
+        {
+          duration: 10,
+          boxMaterialColor: "#0C0400",
+        },
+
+        "<"
+      )
+      .to(pivot.rotation, {
+        duration: 10,
+        y: Math.PI,
+      })
+      .to(
+        three.camera.position,
+        {
+          duration: 10,
+          x: -4,
+          z: 12,
+        },
+        "<"
+      )
       .to(three.camera.position, {
         duration: 10,
         x: 0,
@@ -121,8 +230,38 @@ export const Dancer = () => {
         duration: 10,
         x: 0,
         z: 16,
-      });
-  }, [isEntered, three.camera.position]);
+        onUpdate: () => {
+          setRotateFinished(false);
+        },
+      })
+      .to(hemisphereLightRef.current, {
+        duration: 5,
+        intensity: 30,
+      })
+      .to(
+        pivot.rotation,
+        {
+          duration: 15,
+          y: Math.PI * 4,
+          onUpdate: () => {
+            setRotateFinished(true);
+          },
+        },
+        "<"
+      )
+      .to(
+        colors,
+        {
+          duration: 15,
+          boxMaterialColor: "#DC4F00",
+        },
+        "<"
+      );
+
+    return () => {
+      three.scene.remove(pivot);
+    };
+  }, [isEntered, three.camera, three.scene]);
 
   if (isEntered) {
     return (
