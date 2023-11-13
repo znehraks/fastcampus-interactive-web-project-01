61c773cac83e9463a60278da05c5bc86e0678b93
znehraks <znehraks@gmail.com>
2023-11-13 22:01:51 +0900

    06 gsap 알아보기

--- chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
+++ chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/no-unknown-property */
 import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
+import gsap from "gsap";
+
 export const Dancer = () => {
   const isEntered = useRecoilValue(IsEnteredAtom);
+  const three = useThree();
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("/models/dancer.glb"); // useGLTF Hook으로 모델링 로드
   const { actions } = useAnimations(animations, dancerRef); // useAnimations Hook으로 애니메이션 로드
@@ -15,12 +18,43 @@ export const Dancer = () => {
   const scroll = useScroll(); // ScrollControls 하위에 있는 컴포넌트가 사용 가능한 hook
   useFrame(() => {
     console.log(scroll.offset); // scroll.offset을 통해, 현재 스크롤된 offset을 알 수 있다.(스크롤을 하나도 안하면 0, 끝까지 하면 1)
+    if (!isEntered) return;
   });
 
   useEffect(() => {
     if (!isEntered) return;
     actions["wave"].play(); // 'wave'라는 이름의 애니메이션 재생
   }, [actions, isEntered]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+    gsap.fromTo(
+      three.camera.position,
+      {
+        x: -5,
+        y: 5,
+        z: 5,
+      },
+      {
+        duration: 2.5,
+        x: 0,
+        y: 6,
+        z: 12,
+      }
+    );
+    gsap.fromTo(
+      three.camera.rotation,
+      {
+        z: Math.PI,
+      },
+      {
+        duration: 2.5,
+        z: 0,
+      }
+    );
+  }, [isEntered, three.camera.position, three.camera.rotation]);
+
   if (isEntered) {
     return (
       <>
