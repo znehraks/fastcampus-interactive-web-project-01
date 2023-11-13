70a9c4e992dc37c82ced9a91c98d242873d5b3aa
znehraks <znehraks@gmail.com>
2023-11-14 00:57:58 +0900

    09 스크롤 이벤트로 오브젝트 제어하기

--- chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
+++ chapter04-R3F로-힙합-댄싱-스크롤-페이지-만들기/src/components/Dancer.jsx
@@ -1,22 +1,47 @@
 /* eslint-disable react/no-unknown-property */
-import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
+import {
+  Box,
+  Circle,
+  Points,
+  useAnimations,
+  useGLTF,
+  useScroll,
+  useTexture,
+} from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
 import gsap from "gsap";
+import * as THREE from "three";
 
 let timeline;
 export const Dancer = () => {
   const isEntered = useRecoilValue(IsEnteredAtom);
   const three = useThree();
-  const dancerRef = useRef(null);
+  const scroll = useScroll(); // ScrollControls 하위에 있는 컴포넌트가 사용 가능한 hook
   const { scene, animations } = useGLTF("/models/dancer.glb"); // useGLTF Hook으로 모델링 로드
+  const texture = useTexture("/textures/5.png");
+  const dancerRef = useRef(null);
+  const boxRef = useRef(null);
+  const starGroupRef01 = useRef(null);
+  const starGroupRef02 = useRef(null);
+  const starGroupRef03 = useRef(null);
+  const rectAreaLightRef = useRef(null);
+  const hemisphereLightRef = useRef(null);
   const { actions } = useAnimations(animations, dancerRef); // useAnimations Hook으로 애니메이션 로드
   console.log(actions);
+  const { positions } = useMemo(() => {
+    const count = 500;
+    const positions = new Float32Array(count * 3);
 
-  const scroll = useScroll(); // ScrollControls 하위에 있는 컴포넌트가 사용 가능한 hook
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 25;
+    }
+    return { positions };
+  }, []);
+
   useFrame(() => {
     if (!isEntered) return;
     timeline.seek(scroll.offset * timeline.duration());
@@ -102,8 +127,82 @@ export const Dancer = () => {
   if (isEntered) {
     return (
       <>
-        <ambientLight intensity={2} />
         <primitive ref={dancerRef} object={scene} scale={0.05} />
+        <ambientLight intensity={2} />
+        <rectAreaLight
+          ref={rectAreaLightRef}
+          position={[0, 10, 0]}
+          intensity={30}
+        />
+        <pointLight
+          position={[0, 5, 0]}
+          intensity={45}
+          castShadow
+          receiveShadow
+        />
+        <hemisphereLight
+          ref={hemisphereLightRef}
+          position={[0, 5, 0]}
+          intensity={0}
+          groundColor={"lime"}
+          color={"blue"}
+        />
+
+        <Box ref={boxRef} position={[0, 0, 0]} args={[100, 100, 100]}>
+          <meshStandardMaterial color={"#DC4F00"} side={THREE.DoubleSide} />
+        </Box>
+
+        <Circle
+          castShadow
+          receiveShadow
+          args={[8, 32, 32]}
+          rotation-x={-Math.PI / 2}
+          position-y={-4.4}
+        >
+          <meshStandardMaterial color={"#DC4F00"} side={THREE.DoubleSide} />
+        </Circle>
+        {/* 셰이더 조작해서 반짝이도록 하는 효과 추가 */}
+        <Points positions={positions.slice(0, positions.length / 3)}>
+          <pointsMaterial
+            ref={starGroupRef01}
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
+        <Points
+          positions={positions.slice(
+            positions.length / 3,
+            (positions.length * 2) / 3
+          )}
+        >
+          <pointsMaterial
+            ref={starGroupRef02}
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
+        <Points positions={positions.slice((positions.length * 2) / 3)}>
+          <pointsMaterial
+            ref={starGroupRef03}
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
       </>
     );
   }
